0eba9f3f8e4f9e2c938bc380ce871918fd80642f
Junior <juniormbaya243@gmail.com>
2022-06-29 19:25:45 +0100

    create a new search icon

--- components/Header/InputSearch/HeaderInputSearch.js
+++ components/Header/InputSearch/HeaderInputSearch.js
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import React from "react";
+import SearchIcon from "../../../public/searchicon.svg";
 
 const HeaderInputSearch = () => (
-  <input
-    data-testid="input-search"
-    className="inp-icon"
-    placeholder="Rechercher un film"
-  />
+  <div className="block-input-header">
+    <input
+      data-testid="input-search"
+      className="inp-icon"
+      placeholder="Rechercher un film"
+    />
+    <div className="search-icon-input">
+      <Image src={SearchIcon} alt="icon search" width={20} height={20} />
+    </div>
+  </div>
 );
 
 export default HeaderInputSearch;
